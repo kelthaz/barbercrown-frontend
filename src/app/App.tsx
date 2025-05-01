693127c3877c07db242901d01da3ca1ad73e840b
Carlos Prado <carlos.prado@softgic.co>
2025-05-01 14:56:35 -0500

    Correccion rutas par despliegue

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-// src/app/App.tsx
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '../features/auth/pages/LoginPage';
 import DashboardPage from '../features/dashboard/pages/DashboardPage';
 import PrivateRoute from '../shared/components/PrivateRoute';
 import AppointmentsDashboard from '../features/appointments/pages/AppointmentsDashboard';
 import DashboardShell from '../shared/components/layout/DashboardShell';
-import { ColorModeProvider } from '../theme/theme'; // Ajusta la ruta
+import { ColorModeProvider } from '../theme/theme';
 
 const Home = () => {
   return <h1 className="text-3xl text-center mt-20">🏠 Página principal</h1>;
@@ -13,15 +12,17 @@ const Home = () => {
 
 export default function App() {
   return (
-    <ColorModeProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardShell />}>
-          <Route index element={<DashboardPage />} />
-          <Route path="appointments" element={<AppointmentsDashboard />} />
-        </Route>
-      </Routes>
-    </ColorModeProvider>
+    <BrowserRouter>
+      <ColorModeProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardShell />}>
+            <Route index element={<DashboardPage />} />
+            <Route path="appointments" element={<AppointmentsDashboard />} />
+          </Route>
+        </Routes>
+      </ColorModeProvider>
+    </BrowserRouter>
   );
 }

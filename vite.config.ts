da6fc98d44266786251444f204e14ef3257eb6e1
Carlos Prado <carlos.prado@softgic.co>
2025-05-01 14:49:02 -0500

    Correccion en despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'build', // La carpeta de salida de Vite
     rollupOptions: {

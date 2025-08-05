bedfd475feef6d5cb12d4042bd7deb3ebee500b7
Carlos David Prado Hernandez <davidkelthaz@gmail.com>
2025-08-05 17:09:34 -0500

    Se consume servicio para crear roles

--- src/features/roles/pages/roleDashboard.tsx
+++ src/features/roles/pages/roleDashboard.tsx
@@ -1,19 +1,49 @@
 import RolesTable from '../components/roleTable';
 import { Box, Typography, Paper, Grid } from '@mui/material';
+import { Roles } from '../types/roles';
+import { fetchRoles } from '../services/roleService';
+import { useState, useEffect } from 'react';
+import RoleForm from '../components/roleForm'
 
 
 export default function RoleDashboard() {
+    const [roles, setRoles] = useState<Roles[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [roleToEdit, setRoleToEdit] = useState<Roles | null>(null);
+
+    const handleAddRole = async (newRole: Roles) => {
+        await loadRoles();
+        setRoleToEdit(null);
+    };
+
+    const loadRoles = async () => {
+        try {
+            setLoading(true);
+            const data = await fetchRoles();
+            setRoles(data);
+        } catch (error) {
+            console.error("Error loading roles", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        loadRoles();
+    }, []);
     return (
         <Box sx={{ p: { xs: 0, sm: 3 }, width: '100%' }}>
-            <Typography variant="h4" component="h1" gutterBottom align="center">
-                👤 lista de roles
-            </Typography>
             <Grid container direction="column" alignItems="center" spacing={3}>
-                <div style={{ width: '100%' }}>
+                <Paper sx={{ p: 3, bgcolor: (theme) => theme.palette.mode === 'dark' ? 'grey.800' : 'white', boxShadow: 1 }}>
+                    <RoleForm onAdd={handleAddRole} roleToEdit={roleToEdit} />
+                </Paper>
+                <Grid container direction="column" alignItems="center" spacing={3}>
                     <div style={{ width: '100%' }}>
-                        <RolesTable />
+                        <div style={{ width: '100%' }}>
+                            <RolesTable roles={roles} />
+                        </div>
                     </div>
-                </div>
+                </Grid>
             </Grid>
         </Box>
     );
--- src/features/roles/services/roleService.ts
+++ src/features/roles/services/roleService.ts
@@ -1,7 +1,14 @@
-import axios from "axios";
 import { Roles } from "../types/roles";
+import api from "../../../shared/app/apiConfig";
+
+const BASE_URL = `${import.meta.env.VITE_APP_API_URL}roles`;
 
 export const fetchRoles = async (): Promise<Roles[]> => {
-  const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}roles`);
+  const response = await api.get(`${import.meta.env.VITE_APP_API_URL}roles`);
+  return response.data;
+};
+
+export const createRole = async (roleData: Partial<Roles>): Promise<Roles> => {
+  const response = await api.post(BASE_URL, roleData);
   return response.data;
 };
